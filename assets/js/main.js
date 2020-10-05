e6841256f7d6653a8a2ece89275f8947a4a6f5c4
luke-tsai <luke.s.tsai@gmail.com>
2020-10-04 23:43:36 -0400

    hide personal section WIP for initial publication

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,16 +50,17 @@
 	// Nav.
 		var $nav = $header.children('nav'),
 			$nav_li = $nav.find('li');
-        /*
+
+		/*
 		// Add "middle" alignment classes if we're dealing with an even number of items.
 			if ($nav_li.length % 2 == 0) {
 
 				$nav.addClass('use-middle');
-				$nav_li.eq( ($nav_li.length / 2) ).addClass('is-middle');
+				$nav_li.eq( ($nav_li.length / 2)).addClass('is-middle');
 
 			}
 		*/
-
+		
 	// Main.
 		var	delay = 325,
 			locked = false;
